ed349745f4d672d518525254583962fba99ffc3e
ftrindadj <ftjustino21@gmail.com>
2026-04-09 08:55:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
-document.getElementById('login-form').addEventListener('submit', async (event) => {
-    event.preventDefault(); // Evita o recarregamento da página
+document.getElementById('login-form').addEventListener('submit', async (event) =>{
+    event.preventDefault(); //aqui evita que a pagina recarregue sozinha
 
     const usernameInput = document.getElementById('username').value;
-    const passwordInput = document.getElementById('password').value;
-    const resultMsg = document.getElementById('result');
+    const passwordInput = document.getElementById('password').value
+    const resultMensagem = document.getElementById('result');
     const loginSection = document.getElementById('login-section');
     const container = document.getElementById('container-principal');
 
-    try {
-        const response = await fetch('http://localhost:3000/login', {
+    try{
+        const response = await fetch ('http://localhost:3000/login', {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username: usernameInput, password: passwordInput })
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ username: usernameInput, password: passwordInput})
         });
 
         const data = await response.json();
 
-        if (response.status === 200) {
-            // Sucesso: Esconde o formulário e cria o Dashboard
+        if(response.status === 200){
+            //sucesso ao entra na conta, esconde o formulario e cria o dashboard
             loginSection.style.display = 'none';
-            resultMsg.innerHTML = ''; // Limpa mensagens de erro anteriores
-            
+            resultMensagem.innerHTML = ''; //limpa os campos anteriores
+
             container.innerHTML = `
-                <div class="success-panel">
-                    <h2>Login bem-sucedido!</h2>
-                    <p>Bem-vindo, <strong>${data.name}</strong>!</p>
-                </div>
+            <div class = "sucess-panel">
+                <h2>Login bem sucedido!</h2>
+                <p>Bem-vindo, <strong>${data.name}</strong>!</p>
+            </div>
             `;
-        } else {
-            // Erro: Exibe mensagem na classe .error
-            resultMsg.innerHTML = `<span class="error">${data.error}</span>`;
+        }else{ //caso de o erro 401
+            resultMensagem.innerHTML = `<span class="error">${data.error}</span>`;
         }
-    } catch (err) {
-        resultMsg.innerHTML = `<span class="error">Erro ao conectar com o servidor.</span>`;
+    } catch (error){
+        resultMensagem.innerHTML = `<span class = "error"> Erro ao conectar com o servidor. </span>`;
     }
 });
