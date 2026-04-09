9c231610d00d7ef06a9481c789a1f1c4091b99cc
ftrindadj <ftjustino21@gmail.com>
2026-04-09 08:55:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,23 +5,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Simulação de Banco de Dados
+//simulação de banco de dados
 const users = [
-    { username: 'admin', password: '123', name: 'Administrador' },
-    { username: 'aluno', password: '456', name: 'Estudante' }
+    { username: 'admin', password: '123', name: 'Administrador'},
+    { username: 'aluno', password: '456', name: 'Estudante'}
 ];
 
+//requisitos para fazer o login no site
 app.post('/login', (req, res) => {
-    const { username, password } = req.body;
+    const { username, password, } = req.body;
     const user = users.find(u => u.username === username && u.password === password);
 
-    if (user) {
-        // Sucesso: Status 200
-        return res.status(200).json({ name: user.name });
+    //verificando se o usuário consta no banco de dados
+    if (user){
+        //sucesso ao entrar: status 200
+        return res.status(200).json({
+            message: 'Sucesso',
+            name: user.name
+        });
     } else {
-        // Erro: Status 401
-        return res.status(401).json({ error: 'Credenciais inválidas' });
+        //Erro ao logar
+        return res.status(401).json({ error: 'Credenciais invalidas' });
     }
 });
 
-app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
+const PORT = 3000;
+app.listen(3000, () => console.log (`'Servidor rodando na porta ${PORT}`));
